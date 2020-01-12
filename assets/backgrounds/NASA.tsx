271f2312a9f4277d19640bf5b22427f88261f69e
Dude for realsies <DuendeInexistente@gmail.com>
2020-01-12 08:55:30 -0300

    First commit. Only beggining area done. 	new file:   NASA.gbsproj 	new file:   NASA.gbsproj.bak 	new file:   assets/backgrounds/1.png 	new file:   assets/backgrounds/1.tmx 	new file:   assets/backgrounds/2.png 	new file:   assets/backgrounds/2.tmx 	new file:   assets/backgrounds/NASA.tsx 	new file:   assets/backgrounds/placeholder.png 	new file:   assets/backgrounds/tileset.aseprite 	new file:   assets/backgrounds/tileset.pn 	new file:   assets/music/Darkstone.mod 	new file:   assets/music/template.mod 	new file:   assets/sprites/actor.png 	new file:   assets/sprites/actor_animated.png 	new file:   assets/sprites/falling nasa.ase 	new file:   assets/sprites/falling nasa.png 	new file:   assets/sprites/falling ship.aseprite 	new file:   assets/sprites/falling ship.png 	new file:   assets/sprites/halving door.aseprite 	new file:   assets/sprites/halving door.png 	new file:   assets/sprites/nasa.ase 	new file:   assets/sprites/nasa.png 	new file:   assets/sprites/smoke.ase 	new file:   assets/sprites/smoke.png 	new file:   assets/sprites/spinning door.aseprite 	new file:   assets/sprites/spinning door.png 	new file:   assets/sprites/static.png 	new file:   assets/ui/ascii orig.png 	new file:   assets/ui/ascii.png 	new file:   assets/ui/cursor.png 	new file:   assets/ui/emotes.png 	new file:   assets/ui/frame.png 	new file:   build/rom/game.gb 	new file:   build/rom/game.map 	new file:   build/rom/game.sym 	new file:   build/rom/kissu2.mp4 	new file:   build/rom/testarea2.mp4 	new file:   build/rom/testarea2.webm 	new file:   build/rom/testarea2vp8.web

---
+++ assets/backgrounds/NASA.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="NASA" tilewidth="8" tileheight="8" tilecount="2500" columns="50">
+ <image source="tileset.png" width="400" height="400"/>
+</tileset>
