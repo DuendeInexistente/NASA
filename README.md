aef97e0e11a792cba28174ab18aabd57829d716f
Dude for realsies <DuendeInexistente@gmail.com>
2020-01-14 06:03:18 -0300

    Progress on the house.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [To download the rom, click here.](https://github.com/DuendeInexistente/NASA/raw/master/build/web/rom/game.gb)
 
-[To play online, you want to go here](/build/web)
+[To play online, you want to go here](https://duendeinexistente.github.io/NASA/build/web/)
 
 I guess you shouldn’t be here, sort of. But you can be. I can’t stop you, now can I?
 I have, though, temporarily encrypted the project file. This is just so that people can’t simply open it and open the game inside out. I’ll make it accesible in a while, though not just now.
---
+++ assets/backgrounds/nasa pieces.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="nasa pieces" tilewidth="96" tileheight="16" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../sprites/Pure white.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../sprites/GBlight.png"/>
+ </tile>
+ <tile id="2">
+  <image width="48" height="16" source="../sprites/falling ship.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../sprites/345458043947057172.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../sprites/nothing.png"/>
+ </tile>
+ <tile id="5">
+  <image width="48" height="16" source="../sprites/falling ship 90.png"/>
+ </tile>
+ <tile id="6">
+  <image width="96" height="16" source="../sprites/nasa.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="16" source="../sprites/halving door.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="16" source="../sprites/spinning door.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="16" source="../sprites/falling nasa.png"/>
+ </tile>
+ <tile id="10">
+  <image width="48" height="16" source="../sprites/smoke.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../sprites/static.png"/>
+ </tile>
+ <tile id="12">
+  <image width="48" height="16" source="../sprites/actor.png"/>
+ </tile>
+ <tile id="13">
+  <image width="96" height="16" source="../sprites/actor_animated.png"/>
+ </tile>
+</tileset>
